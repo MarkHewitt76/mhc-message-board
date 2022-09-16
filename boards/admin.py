1c09a9a6e404e83022cb073e78312da78a5a9d9b
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-16 00:42:42 +0000

    Add Post model to admin page and format. Add Summernote WYSIWYG editor

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Adds Post model to admin page with search bar,
+    Summernote WYSIWYG editor, formatted fields and
+    filter panels. Slug field prepopulated from title.
+    """
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
