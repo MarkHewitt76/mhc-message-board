cc1b1e6257aa694322cd6ef1c5bdc03263c3e595
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-16 03:12:57 +0000

    Create PostList view class, update urls and add boilerplate templates

--- boards/urls.py
+++ boards/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='boards-home'),
-    path('about/', views.about, name='boards-about'),
+    path('', views.PostList.as_view(), name='boards_home'),
+    # path('about/', views.about, name='boards-about'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+# from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('<h1>Homepage</h1>')
+class PostList(generic.ListView):
+    """
+    View for all published posts, in descending order, using
+    Post model and inheriting from generic list view model.
+    To be used on homepage.
+    """
+
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+# def home(request):
+#     return HttpResponse('<h1>Homepage</h1>')
 
 
-def about(request):
-    return HttpResponse('<h1>About page</h1>')
+# def about(request):
+#     return HttpResponse('<h1>About page</h1>')
