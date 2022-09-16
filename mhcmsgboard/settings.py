afa2b3da8ff464f8a8d53ff634b5280190122c80
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-16 21:10:03 +0000

    Install crispy forms

--- mhcmsgboard/settings.py
+++ mhcmsgboard/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'boards',
 ]
 
@@ -142,3 +143,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms template pack for Bootstrap
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
