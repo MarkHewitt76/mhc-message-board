84c8060d6420342ad6d67d2f77461f306d128f5d
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-17 03:38:13 +0000

    Create model form classes to allow updating of user and profile info on the front end

--- boards/forms.py
+++ boards/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -15,3 +16,28 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Model form class which inherits the ModelForm object from forms.
+    Will allow the user to update their info in the User model
+    from the front end.
+    """
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """
+    Model form class which inherits the ModelForm object from forms.
+    Will allow the user to upload a profile picture to the UserProfile
+    model from the front end.
+    """
+    class Meta:
+        model = UserProfile
+        fields = ['image']
