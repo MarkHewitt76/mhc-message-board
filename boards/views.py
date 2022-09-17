a62f83c7760af1b29ef9078609bba88bbabd22e0
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-17 23:10:00 +0000

    Add pagination to homepage

--- boards/views.py
+++ boards/views.py
@@ -22,6 +22,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 6
 
 
 class FullPost(View):
