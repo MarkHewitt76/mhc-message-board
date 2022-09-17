88f823dd3e5abbfb8364cdcf8019e20cb4c0e6b9
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-17 14:13:00 +0000

    Fix broken external image links and incorrect profile picture views

--- mhcmsgboard/urls.py
+++ mhcmsgboard/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from boards import views
 
 urlpatterns = [
@@ -33,6 +31,3 @@ urlpatterns = [
         ), name='logout'),
     path('', include('boards.urls'), name='boards_urls'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
