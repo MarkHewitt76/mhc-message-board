4952d0f2618bf4ac947062edcd9417016dfc9a47
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-20 14:50:12 +0000

    Add context processor for Category model and add code to base.html template to dynamically populate navbar dropdown

---
+++ boards/context_processors.py
@@ -0,0 +1,10 @@
+from .models import Category
+
+
+def get_categories(request):
+    """
+    Context processor code to make Category model dynamic
+    in all templates. Needed to populate nav in base.html.
+    """
+
+    return {"categories": Category.objects.all()}
