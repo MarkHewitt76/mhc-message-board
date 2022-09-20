c3a5b4804462c45abeee7f7fe4a5723202663ade
MarkHewitt76 <markyhewitt@gmail.com>
2022-09-20 09:57:18 +0000

    Refactor pagination buttons into separate template. Fix paths and location of boards routes

--- boards/urls.py
+++ boards/urls.py
@@ -1,7 +1,35 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='boards_home'),
-    path('<slug:slug>/', views.FullPost.as_view(), name='boards_post'),
+    path('post/<slug:slug>/', views.FullPost.as_view(), name='boards_post'),
+    path('register/', views.register, name='register'),
+    path('user/profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html'
+        ),
+        name='login'
+    ),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='logout.html'
+        ),
+        name='logout'
+    ),
+    path('new/', views.CreatePost.as_view(
+        template_name='post_form.html'
+        ),
+        name='create_post'
+    ),
+    path('post/<slug:slug>/update/', views.UpdatePost.as_view(
+        template_name='post_form.html'
+        ),
+        name='update_post'
+    ),
+    path('post/<slug:slug>/delete/', views.DeletePost.as_view(
+        template_name='post_confirm_delete.html'
+        ),
+        name='delete_post'
+    ),
 ]
--- mhcmsgboard/urls.py
+++ mhcmsgboard/urls.py
@@ -14,39 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from boards import views
+# from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='login.html'
-        ),
-        name='login'
-    ),
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='logout.html'
-        ),
-        name='logout'
-    ),
-    path('new/', views.CreatePost.as_view(
-        template_name='post_form.html'
-        ),
-        name='create_post'
-    ),
-    path('<slug:slug>/update/', views.UpdatePost.as_view(
-        template_name='post_form.html'
-        ),
-        name='update_post'
-    ),
-    path('<slug:slug>/delete/', views.DeletePost.as_view(
-        template_name='post_confirm_delete.html'
-        ),
-        name='delete_post'
-    ),
     path('', include('boards.urls'), name='boards_urls'),
 ]
